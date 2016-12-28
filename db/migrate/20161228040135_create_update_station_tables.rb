ac55edb5933f18f2cd651449f9f5fae8a3ebcb34
dankim93 <dan_kim93@hotmail.com>
2016-12-27 21:07:55 -0800

    Seed travel times for embr

--- db/migrate/20161228040135_create_update_station_tables.rb
+++ db/migrate/20161228040135_create_update_station_tables.rb
@@ -1,6 +1,8 @@
 class CreateUpdateStationTables < ActiveRecord::Migration
   def change
     create_table :update_station_tables do |t|
+      add_column :stations, :name, :string
+      add_column :stations, :abbr, :string
 
       t.timestamps null: false
     end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,38 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+##########################
+# SEED bart_travel_times #
+##########################
+
+## from embarcadero
+BartTravelTime.create(start: 'embr', end: 'woak', time_in_min: 7)
+BartTravelTime.create(start: 'embr', end: '12th', time_in_min: 11)
+BartTravelTime.create(start: 'embr', end: '19th', time_in_min: 12)
+BartTravelTime.create(start: 'embr', end: 'mcar', time_in_min: 16)
+BartTravelTime.create(start: 'embr', end: 'ashb', time_in_min: 19)
+BartTravelTime.create(start: 'embr', end: 'dbrk', time_in_min: 21)
+BartTravelTime.create(start: 'embr', end: 'nbrk', time_in_min: 24)
+BartTravelTime.create(start: 'embr', end: 'plza', time_in_min: 27)
+BartTravelTime.create(start: 'embr', end: 'deln', time_in_min: 30)
+BartTravelTime.create(start: 'embr', end: 'rich', time_in_min: 34)
+BartTravelTime.create(start: 'embr', end: 'rock', time_in_min: 20)
+BartTravelTime.create(start: 'embr', end: 'orin', time_in_min: 25)
+BartTravelTime.create(start: 'embr', end: 'lafy', time_in_min: 30)
+BartTravelTime.create(start: 'embr', end: 'wcrk', time_in_min: 34)
+BartTravelTime.create(start: 'embr', end: 'phil', time_in_min: 37)
+BartTravelTime.create(start: 'embr', end: 'conc', time_in_min: 42)
+BartTravelTime.create(start: 'embr', end: 'ncon', time_in_min: 46)
+BartTravelTime.create(start: 'embr', end: 'pitt', time_in_min: 52)
+BartTravelTime.create(start: 'embr', end: 'lake', time_in_min: 12)
+BartTravelTime.create(start: 'embr', end: 'ftvl', time_in_min: 16)
+BartTravelTime.create(start: 'embr', end: 'cols', time_in_min: 19)
+BartTravelTime.create(start: 'embr', end: 'sanl', time_in_min: 23)
+BartTravelTime.create(start: 'embr', end: 'bayf', time_in_min: 27)
+BartTravelTime.create(start: 'embr', end: 'cast', time_in_min: 31)
+BartTravelTime.create(start: 'embr', end: 'dubl', time_in_min: 44)
+BartTravelTime.create(start: 'embr', end: 'hayw', time_in_min: 37)
+BartTravelTime.create(start: 'embr', end: 'shay', time_in_min: 41)
+BartTravelTime.create(start: 'embr', end: 'ucty', time_in_min: 46)
+BartTravelTime.create(start: 'embr', end: 'frmt', time_in_min: 51)
