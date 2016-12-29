3e1b7cc8934e5c4442dc4ee61f06c9ef2ecaab3d
Igor Dmitriev <igor.s.dmitriev@gmail.com>
2016-12-28 16:51:28 -0800

    Add destination form, styling and logic

--- frontend/components/chooseDirections.jsx
+++ frontend/components/chooseDirections.jsx
@@ -11,7 +11,56 @@ class ChooseDirectionsForm extends React.Component {
     };
 
     this.startStations =
-      ['Embracadero', 'Montgomery St', 'Powell St', 'Civic Center'];
+      ['Embracadero',
+       'Montgomery St',
+       'Powell St',
+       'Civic Center'];
+
+    this.destinationStations =
+      ["12th St. Oakland City Center",
+       "16th St. Mission (SF)",
+       "19th St. Oakland",
+       "24th St. Mission (SF)",
+       "Ashby (Berkeley)",
+       "Balboa Park (SF)",
+       "Bay Fair (San Leandro)",
+       "Castro Valley",
+       "Civic Center (SF)",
+       "Coliseum",
+       "Colma",
+       "Concord",
+       "Daly City",
+       "Downtown Berkeley",
+       "Dublin/Pleasanton",
+       "El Cerrito del Norte",
+       "El Cerrito Plaza",
+       "Embarcadero (SF)",
+       "Fremont",
+       "Fruitvale (Oakland)",
+       "Glen Park (SF)",
+       "Hayward",
+       "Lafayette",
+       "Lake Merritt (Oakland)",
+       "MacArthur (Oakland)",
+       "Millbrae",
+       "Montgomery St. (SF)",
+       "North Berkeley",
+       "North Concord/Martinez",
+       "Oakland Int'l Airport",
+       "Orinda",
+       "Pittsburg/Bay Point",
+       "Pleasant Hill",
+       "Powell St. (SF)",
+       "Richmond",
+       "Rockridge (Oakland)",
+       "San Bruno",
+       "San Francisco Int'l Airport",
+       "San Leandro",
+       "South Hayward",
+       "South San Francisco",
+       "Union City",
+       "Walnut Creek",
+       "West Oakland"];
 
     // this.handleStartChange = this.handleStartChange.bind(this);
     this.handleDestinationChange = this.handleDestinationChange.bind(this);
@@ -28,7 +77,8 @@ class ChooseDirectionsForm extends React.Component {
   }
 
   handleDestinationChange(event) {
-
+    event.preventDefault();
+    this.setState({ destination: event.target.value });
   }
 
   handleSubmit(event) {
@@ -46,14 +96,32 @@ class ChooseDirectionsForm extends React.Component {
                 </button>);
     });
 
-    let destinationOptions = [];
+    let destinationOptions = this.destinationStations.map(station => (
+      <option value={station} key={station}>{station}</option>
+    ));
 
+    let selectValue, selectClass;
+    console.log(this.state.destination);
+    if (this.state.destination === "") {
+        selectValue = "Select Station";
+        selectClass = "empty";
+      } else {
+        selectValue = this.state.destination;
+        selectClass = "selected";
+      }
     return (
       <form className="choose-directions-form" >
         <h3>Where you departing from?</h3>
         {startButtons}
         <h3>Where is your destination?</h3>
-        {destinationOptions}
+        <div className="select-destination">
+          <select value={selectValue}
+                  className={selectClass}
+                  onChange={this.handleDestinationChange}>
+            <option value="Select Station" disabled>Select Station</option>
+            {destinationOptions}
+          </select>
+        </div>
         <span>Do your magic and</span>
         <button onClick={this.handleSubmit}>Show me the options</button>
       </form>
